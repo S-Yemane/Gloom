37718e4d9d151cf3d9acb64f6ffa4e8a752cdadb
dlnew3 <dlnew3@gmail.com>
2020-02-16 23:58:39 -0800

    3D player controls
    Created enclosed area for player to move around.
    Player object. Camera Control. wasd movement.

---
+++ Assets/Camera_Control.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxis axes = RotationAxis.MouseX;
+
+    public float sensHorizontal = 10.0f;
+    public float sensVertical = 10.0f;
+
+    public float _rotationX = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(axes == RotationAxis.MouseX)
+        {
+            transform.Rotate (0, Input.GetAxis ("Mouse X") * sensHorizontal, 0);
+        }
+        else if (axes == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
