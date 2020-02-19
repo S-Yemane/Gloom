76cec7b8585170ab05c3134a15967f45e02e6c04
Daniel Banuelos <banuelos.dan@csu.fullerton.edu>
2020-02-19 15:28:02 -0800

    2-19
    test-commit

---
+++ Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    { //called before performing any physics here. 
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (moveHorizontal == 3)
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+
+    }
+    //which ever objects that have trigger enabled will cause the scene to change
+    void OnTriggerEnter(Collider other)
+    {
+        
+        Application.LoadLevel(1);
+
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # GLOOM is a First-Person Shooter video game developed using Unity. Gloom utilizes random-generated maps to provide a unique experience
 # for players.
 #
-# 
+#
