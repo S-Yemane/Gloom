72ff6a5ac2bba73f5f3f386192a673cc6ef19c1e
Daniel Banuelos <banuelos.dan@csu.fullerton.edu>
2020-02-26 16:55:55 -0800

    PickUps/Teleporter
    I added two pick-up objects, one to alter the speed and the other to change the player color. I also have a teleporting mechanism working.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    float speedTimer = 0.0f;
+    bool endEffect = false;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+       
+            }
+    void Update() {
+        if (endEffect) {
+            speedTimer += Time.deltaTime;
+            if (speedTimer > 5.0f) {
+                speed = 10;
+                endEffect = false;
+                speedTimer = 0.0f;
+            }
+        }
+    }
+    void FixedUpdate() { //called before performing any physics here. 
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //create a tag to the pickup prefab & the string has to match up to the tag we create
+        if (other.gameObject.CompareTag("SpeedPerk"))
+        {
+            other.gameObject.SetActive(false);
+            speed = 40;
+            endEffect = true;
+        }
+        if (other.gameObject.CompareTag("ColorPerk"))
+        {
+            other.gameObject.SetActive(false);
+            GetComponent<Renderer>().material.color = Color.black;
+
+
+
+        }
+    }
+   
+}
