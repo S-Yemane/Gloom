8de067ccc3d5819459bda36a3bfb3c63a60a74c6
dlnew3 <dlnew3@gmail.com>
2020-03-12 02:46:22 -0700

    Asset Changes
    - Removed contents of "Low Poly FPS Pack - Free (Sample)" Asset from Project.
    + Imported "Easy Weapons" Asset Pack into Project.
    +Imported "Standard Assets (for Unity 2017.3)" Asset Pack into Project.
    = Made changes to Player GameObject, modifying the FPSController prefab from Unity Standard Assets.
    = Added Weapon Systems script to Player GameObject.
    = Made positional changes to guns in relation to Camera; positioned similarly to classic DOOM.
    ? Health variables appear to be a script on the "Easy Weapons" Asset Pack, may be able to build a Health System from that.
    ? Bullet Properties easily modifiable on "Easy Weapons".
    
    ! TODO: Add Enemy and Enemy AI to test damage values.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target; //?
+    [SerializeField] float chaseRange = 5f;
+
+
+    NavMeshAgent navMeshAgent; //? need to search up
+    float distanceToTarget = Mathf.Infinity; //
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>(); //??
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position); // measure distance between us and target(player)
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }else if(distanceToTarget <= chaseRange) // if the distance is less than 5f
+        {
+            isProvoked = true;
+        }
+
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);  // set the AI's destination to where our target(player) is (so that its following the player)
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seeked and is destyoying " + target.name);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange); // (mid point, radius)
+    }
+}
